5246a7d0bc30373a02b90cf242ee7d4cc836fdf4
Coding Comets <Diegoalizo75@gmail.com>
2023-11-11 14:31:32 -0500

    The basic operational model of the solar system

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SolarSystem solar = new SolarSystem();
+        solar.sun = "1";
+        solar.planets = "7";
+        solar.moon = "26";
+    }
+}
